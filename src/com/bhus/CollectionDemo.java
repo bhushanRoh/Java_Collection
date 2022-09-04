28be64e9812cc1ab4e2664129b89bd798fee3bd7
bhushanRoh <rohankarbhushan74@gmail.com>
2022-09-04 12:48:22 +0530

    Collection Interface Example

---
+++ src/com/bhus/CollectionDemo.java
@@ -0,0 +1,27 @@
+package com.bhus;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class CollectionDemo {
+    public static void main(String[] args) {
+
+        Collection<String> names = new ArrayList<>();
+        names.add("Bhushan");
+        names.add("Gaurav");
+        names.add("Pranay");
+        names.add("Atharva");
+
+        System.out.println(names);
+
+        names.remove("Pranay");
+
+        System.out.println(names);
+
+        System.out.println(names.contains("Bhushan"));
+
+        names.forEach((name) -> {
+            System.out.println(name);
+        });
+    }
+}
--- src/com/bhus/Main.java
+++
@@ -1,8 +0,0 @@
-package com.bhus;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
